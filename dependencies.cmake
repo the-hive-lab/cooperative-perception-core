06464cc344f5989a80e7645b5ee13988dbfa68ba
Adam Morrissett <morrissettal2@vcu.edu>
2022-11-17 10:40:51 -0500

    Add Doxygen and Sphinx documentation generation
    Doxygen is the de facto standard documentation generator for C++
    projects, but its design leaves a lot to be desired. The Sphinx
    documentation generator has a more modern design. Sphinx does not
    natively work with C++, so there is a plugin called Breathe that bridges
    Doxygen output with Sphinx. This commit contains the configuration files
    needed to build the project documentation using Doxygen and Sphinx.
    
    CMakeLists.txt
    * add configure option to build project documentation
    * conditionally add documentation building to project
    
    dependencies.cmake
    * conditionally add Doxygen as project dependency
    
    docs/CMakeLists.txt
    * add target to build documentation
    
    docs/Doxyfile.in
    * add template file for Doxygen configuration. This file gets filled in
      by CMake at configure time
    
    docs/conf.py
    * add Sphinx configuration file for the project
    
    docs/requirements.txt
    * add Python dependencies needed to build project documentation.
      Documentation generation is mainly a Python process that gets invoked
      by CMake

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,6 +1,10 @@
 find_package(Eigen3 REQUIRED)
 find_package(units REQUIRED)
 
+if(cooperative_perception_BUILD_DOCS)
+  find_package(Doxygen REQUIRED)
+endif()
+
 if(cooperative_perception_ENABLE_TESTING OR PROJECT_IS_TOP_LEVEL)
   find_package(GTest REQUIRED)
 endif()
---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Cooperative Perception - Core'
+copyright = '2022, Leidos'
+author = 'The Human and Intelligent Vehicle Ensembles (HIVE) Lab'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'breathe',
+    'sphinx.ext.mathjax'
+]
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for sphinx output -----------------------------------------------
+
+breathe_default_project = 'cooperative_perception_core'
